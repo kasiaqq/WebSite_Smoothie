b59f21342ce113793315bac23fffbb5fd1305e1a
kasiaqq <katarzynamajcherqq@gmail.com>
2019-03-09 18:19:35 +0100

    Initial commit

---
+++ js.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+
+	// PŁYNNE PRZEJSCIE DO SEKCJI PO KLIKNIECIU W LINK (U MNIE W LOGO)
+	/*  $(document).on('click', 'a', function (event) {
+		if (this.hash !== "") {
+		  event.preventDefault();
+		  var hash = this.hash;
+	  	$('body').animate({
+	  		scrollTop: $(hash).offset().top
+	  	}, 500, function(){
+			  window.location.hash = hash;
+		  });
+		}
+	  }); */
+
+	// MENU
+	$("#menu").click(openMenu);
+	$("#close").click(closeMenu);
+
+	function openMenu() {
+		$("#menuSlide").css({ "width": "20%" });
+		$("#menu").addClass("invisible");
+		$("#close").removeClass("invisible");
+		$(".menuText").removeClass("invisible");
+	}
+	function closeMenu() {
+		$("#menuSlide").css({ "width": "0" });
+		$("#menu").removeClass("invisible");
+		$("#close").addClass("invisible");
+		$(".menuText").addClass("invisible");
+	}
+	// PAGE 1
+	$("#page1").ready(function () {
+		$("#check").addClass("checked");
+	})
+
+	// ANIMACJA
+	AOS.init();
+
+	
+
+});
